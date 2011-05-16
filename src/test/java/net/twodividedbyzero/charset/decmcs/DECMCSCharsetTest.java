0b47015379b44b5f51c76bf445dd6d2c8df6ec65
Nathan Beyer <nbeyer@gmail.com>
2011-05-15 21:38:55 -0500

    more encoding tests

--- src/test/java/net/twodividedbyzero/charset/decmcs/DECMCSCharsetTest.java
+++ src/test/java/net/twodividedbyzero/charset/decmcs/DECMCSCharsetTest.java
@@ -22,6 +22,7 @@ import static org.junit.Assert.assertThat;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.nio.charset.Charset;
+import java.util.Arrays;
 
 import org.junit.Test;
 
@@ -52,24 +53,81 @@ public class DECMCSCharsetTest {
     final Charset iso88591 = Charset.forName("ISO-8859-1");
     assertThat(cs.contains(iso88591), is(false));
   }
-  
+
   @Test
   public void testEncodeString00to9F() {
-    Charset cs = new DECMCSCharset();
+    final Charset cs = new DECMCSCharset();
 
-    char[] chars00To9F = new char[160];
-    byte[] bytes00To9F = new byte[160];
+    final char[] chars00To9F = new char[160];
+    final byte[] bytes00To9F = new byte[160];
     for (char c = '\u0000'; c <= '\u009F'; c++) {
       chars00To9F[c] = c;
       bytes00To9F[c] = (byte) c;
     }
-    String string00To9F = new String(chars00To9F);
-
-    ByteBuffer result = cs.encode(string00To9F);
-    byte[] resultBytes = result.array();
+    final String string00To9F = new String(chars00To9F);
+    assertThat(string00To9F.length(), is(160));
+    final ByteBuffer result = cs.encode(string00To9F);
+    final byte[] resultBytes = result.array();
     for (int i = 0; i < 160; i++) {
-      assertEquals("bytes don't match at index " + i, bytes00To9F[i],
-          resultBytes[i]);
+      assertThat("bytes don't match at index " + i, resultBytes[i], is(bytes00To9F[i]));
+    }
+  }
+
+  @Test
+  public void testEncodeString0AtoFFUnmappable() throws Exception {
+    final Charset cs = new DECMCSCharset();
+    final String unmappable = "\u00A0\u00A6\u00A8\u00AC\u00AD\u00AE\u00AF\u00B4\u00B8\u00BE\u00D0\u00D7\u00DD\u00DE\u00F0\u00F7\u00FD\u00FE";
+    final ByteBuffer result = cs.encode(unmappable);
+    final byte[] resultBytes = result.array();
+
+    assertThat("encoded results aren't the same length as original string", resultBytes.length, is(unmappable.length()));
+
+    for (int i = 0; i < resultBytes.length; i++) {
+      // 0x3F is '?'
+      assertThat("unexpected byte value at index " + i, resultBytes[i], is((byte) 0x3F));
+    }
+  }
+
+  @Test
+  public void testEncodeString0AtoFFDiffFromISO88591() throws Exception {
+    final Charset cs = new DECMCSCharset();
+
+    final String u00A4 = "\u00A4";
+    ByteBuffer result = cs.encode(u00A4);
+    byte[] resultBytes = result.array();
+    assertThat(resultBytes.length, is(1));
+    assertThat(resultBytes[0], is((byte) 0xA8));
+
+    final String u00FF = "\u00FF";
+    result = cs.encode(u00FF);
+    resultBytes = result.array();
+    assertThat(resultBytes.length, is(1));
+    assertThat(resultBytes[0], is((byte) 0xFD));
+  }
+
+  @Test
+  public void testEncodeStringA0toFFSameAsISO88591() throws Exception {
+    final char[] exclude = { '\u00A0', '\u00A4', '\u00A6', '\u00A8', '\u00AC', '\u00AD', '\u00AE', '\u00AF', '\u00B4', '\u00B8',
+        '\u00BE', '\u00D0', '\u00D7', '\u00DD', '\u00DE', '\u00F0', '\u00F7', '\u00FD', '\u00FE', '\u00FF' };
+
+    final char[] charsA0ToFF = new char[76];
+    final byte[] bytesA0ToFF = new byte[76];
+    for (char c = '\u00A0', i = '\u0000'; c <= '\u00FF'; c++) {
+      if (Arrays.binarySearch(exclude, c) < 0) {
+        charsA0ToFF[i] = c;
+        bytesA0ToFF[i] = (byte)c;
+        i++;
+      }
+    }
+    
+    final Charset cs = new DECMCSCharset();
+    
+    final String stringA0ToFF = new String(charsA0ToFF);
+    assertThat(stringA0ToFF.length(), is(76));
+    final ByteBuffer result = cs.encode(stringA0ToFF);
+    final byte[] resultBytes = result.array();
+    for (int i = 0; i < 76; i++) {
+      assertThat("bytes don't match at index " + i, resultBytes[i], is(bytesA0ToFF[i]));
     }
   }
 
